64201c56bfec077122063015e3b2fb25c0353cb3
mohammad-a-hossain <adam.eve007@gmail.com>
2021-12-23 15:37:06 +0600

    product create api done

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require('../models/Product')
 const multer = require('multer')
 const shortId =require('shortid')
-
+const slugify = require('slugify')
 
 
 
@@ -10,5 +10,34 @@ const shortId =require('shortid')
 exports.addProduct =(req,res)=>{
 
         //res.status(200).json({file:req.file, body:req.body})
-        res.status(200).json({file:req.files, body:req.body})
+        //res.status(200).json({file:req.files, body:req.body})
+
+     const {name, price, description,quantity, category,createdBy} = req.body
+
+     let productPics =[]
+
+     if(req.files.length > 0){
+         productPics =req.files.map(file => {
+             return {img:file.filename}
+         })
+     }
+
+     const product = new Product({
+        name:name,
+         price,
+         slug:slugify(name),
+          description,
+          productPics,
+          quantity,
+           category,
+           createdBy:req.user._id
+     })
+
+     product.save(((error,product)=>{
+         if(error) return res.status(400).json({error})
+          if(product){
+              return res.status(201).json({product})
+          }
+     }))
+
     }
--- models/Product.js
+++ models/Product.js
@@ -16,6 +16,10 @@ const productSchem = new mongoose.Schema({
         type:Number,
         required:true
     },
+    quantity:{
+        type:Number,
+        required:true
+    },
     description:{
         type:String,
         required:true,
@@ -36,7 +40,7 @@ const productSchem = new mongoose.Schema({
         }
     ],
     category:{
-        type:mongoose.Schema.Types.ObjectId,ref:"Category"
+        type:mongoose.Schema.Types.ObjectId,ref:"Category",required:true
     },
     createdBy:{
         type:mongoose.Schema.Types.ObjectId, ref:"User"
